757b8455fccedfecffa47f604797f5fe3f3fe157
dweaver-jin <71699905+dweaver-jin@users.noreply.github.com>
2020-09-26 11:38:34 +0800

    Update diy-lede.sh

--- diy-lede.sh
+++ diy-lede.sh
@@ -41,6 +41,7 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/l
 git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
 git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome/
 svn co https://github.com/xiaorouji/openwrt-package/trunk/lienol/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package package/lean/package  #passwall出国软件配套
 
